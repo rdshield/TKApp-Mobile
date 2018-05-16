98e1feacf7c0312d1325b5c53609b893f62f9d57
Byron Padilla <bbpadill@uci.edu>
2018-05-15 18:08:54 -0700

    dfasdf
    dwfasdf

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ export default class App extends React.Component {
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <Text>This was added by me, Byron Padilla.</Text>
+        <Text>This was added by me, Byron Padilla.</Text>
       </View>
     );
   }
